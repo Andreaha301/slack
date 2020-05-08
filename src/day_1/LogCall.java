396507a4b6e1f07b6b4e802ca76fcb8aded112a3
Andreaha301 <64175520+Andreaha301@users.noreply.github.com>
2020-05-08 15:35:41 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,5 +3,9 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("Hello Andrea");
+        
+        
+        
+        //This line is from GitHub
     }
 }
